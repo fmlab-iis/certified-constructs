3bac4ee41c747122a50cf416421ad50bfca08d3d
Bow-Yaw Wang <bywang@iis.sinica.edu.tw>
2017-05-31 11:32:25 +0800

    add snippet for NIST P-256 square

---
+++ examples/ecp_nistz256_sqr/ecp_nistz256_sqr.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+unsigned long a[] = { 0x7f7fbfca0020813a, 0xfaafade709201c18,
+                      0xcd20398728bdee5c, 0x15b5974233712938 };
+
+void ecp_nistz256_sqr_mont(unsigned long[4], unsigned long[4]);
+
+int main()
+{
+    unsigned long r[4];
+
+    ecp_nistz256_sqr_mont(r, a);
+
+    printf("%016lx:%016lx:%016lx:%016lx\n",r[3],r[2],r[1],r[0]);
+}
